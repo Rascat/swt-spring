dce6c84b7d3548398d454fe1308826ebdf2fd736
schmidt_uni_leipzig <jschmidt@informatik.uni-leipzig.de>
2017-12-13 14:37:55 +0100

    [feat] paging and sorting

--- src/main/java/de/ul/swt/spring/repository/PersonRepository.java
+++ src/main/java/de/ul/swt/spring/repository/PersonRepository.java
@@ -1,11 +1,11 @@
 package de.ul.swt.spring.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import de.ul.swt.spring.domain.Person;
 
 @RepositoryRestResource(collectionResourceRel = "person", path = "person")
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
 }

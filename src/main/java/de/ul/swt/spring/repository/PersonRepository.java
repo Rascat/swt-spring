4f080ff82953cae46535d479efc4c3f7c0b18fe4
schmidt_uni_leipzig <jschmidt@informatik.uni-leipzig.de>
2017-12-13 14:46:53 +0100

    [feat] simple seach by name

--- src/main/java/de/ul/swt/spring/repository/PersonRepository.java
+++ src/main/java/de/ul/swt/spring/repository/PersonRepository.java
@@ -1,11 +1,19 @@
 package de.ul.swt.spring.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import de.ul.swt.spring.domain.Person;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "person", path = "person")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    public Page<Person> findByNameStartsWithIgnoreCase(@Param("n") String name,
+            Pageable p);
 
 }
